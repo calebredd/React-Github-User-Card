a4862d4d683979ba0d944bc865e17356839bcabe
Caleb Redd <caleb.redd@yahoo.com>
2019-09-05 17:26:02 -0700

    Implement fetch inside of componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Github Users</h1>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { doggos: [] };
+  }
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breed/labrador/images")
+      .then(res => res.json())
+      .then(dogs => this.setState({ doggos: dogs.message }))
+      .catch(err => console.log("noooo"));
+  }
+  render() {
+    // console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Github Users</h1>
+            </header>
+          <div>
+            {this.state.doggos.map(dogs => (
+              <img src={dogs} alt="Dog" />
+            ))}
+          </div>
+      </div>
+    );
+  }
 }
-
-export default App;
